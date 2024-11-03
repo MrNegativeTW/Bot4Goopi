4df409a952fb1f30a1de821657de5dd84c91db95
Trevor <wowpapa3232@gmail.com>
2024-11-03 23:52:53 +0800

    feat: Now we have tab instead of tons label and box

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,7 +1,10 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 from tkinter import messagebox, font, StringVar
 from bot.bot import launch_bot, open_sign_in_page
 from PIL import Image, ImageTk
+
+from data import constants as Constants
 
 class Application(tk.Tk):
     def __init__(self):
@@ -10,12 +13,14 @@ class Application(tk.Tk):
         self.title("Goopi Bot - v0.0.1")
         self.setupWindow()
 
-        self.createMenu()
+        # self.createMenu()
         # self.createImages()
+        self.create_notebook()
+        self.create_section_pref()
+        self.create_section_autofill()
+        self.create_section_about()
 
-        self.createSectionBasic()
-        self.createSectionDeliver()
-        self.createSectionCreditCard()
+        self.create_footer_actions()
 
     def setupWindow(self):
         w = 400 # width for the Tk root
@@ -32,125 +37,137 @@ class Application(tk.Tk):
         # and where it is placed
         self.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-        self.resizable(False, False)
+        # self.resizable(False, False)
 
     def createMenu(self):
         menu = tk.Menu(self)
-
         menu.add_command(label='載入資料')
         menu.add_command(label='儲存資料')
         menu.add_command(label='注意事項', command=self.show_info_dialog)
         menu.add_command(label='關於', command=self.show_about_dialog)
-
         self.config(menu=menu)
 
-    def show_info_dialog(self):
-        messagebox.showinfo("注意事項", "謝謝你的注意")
+    def create_notebook(self):
+        self.notebook = ttk.Notebook(self, width=300)
+        self.notebook.grid(row=0, column=0)
 
-    def show_about_dialog(self):
-        messagebox.showinfo("About", "Goopi Bot\n" + \
-                            "Version: v0.0.1\n" + \
-                            "Author: Trevor\n" + \
-                            "Made with: LOVE\n")
+    def add_notebook(self, frame, title):
+        self.notebook.add(frame, text=title)
 
-    def createImages(self):
-        banner = Image.open('res/goopi_bot.png') 
-        bannerPhotoImage  = ImageTk.PhotoImage(banner)
+    def create_section_pref(self):
+        frame1 = tk.Frame()
+        font_title = font.Font(family="Helvetica", size=10)
+        font_entry =  font.Font(family="Helvetica", size=10)
 
-        icon_label = tk.Label(self, image=bannerPhotoImage)
-        icon_label.grid(row=0, column=0, columnspan=2)
-        icon_label.image = bannerPhotoImage
-
-    def createSectionBasic(self):
-        font_title = font.Font(family="Helvetica", size=12, weight="bold")
-        font_entry =  font.Font(family="Helvetica", size=12)
-
-        self.start_button = tk.Button(self, text="開啟瀏覽器", width=12, command=self.open_sign_in_page)
-        self.start_button.grid(row=0, column=0, pady=8)
-        self.start_button = tk.Button(self, text="執行", width=12, command=self.start_bot_action)
-        self.start_button.grid(row=0, column=1, pady=8)
-
-        tk.Label(self, text="商品網址", font=font_title).grid(row=1, column=0)
-        self.entry_link = tk.Entry(self, font=font_entry)
+        tk.Label(frame1, text="商品網址", font=font_title).grid(row=1, column=0)
+        self.entry_link = tk.Entry(frame1, font=font_entry, width=30)
         self.entry_link.insert(tk.END, "https://www.goopi.co/products/%E2%80%9Crve-s3%E2%80%9D-riverside-track-shorts-pure-black")
         self.entry_link.grid(row=1, column=1)
-        self.button_clear_link = tk.Button(self, text=" 清除 ", command=self.clear_link_entry)
-        self.button_clear_link.grid(row=1, column=3, padx=4)
 
-        tk.Label(self, text="顏色", font=font_title).grid(row=2, column=0)
+        tk.Label(frame1, text="顏色", font=font_title, justify="right").grid(row=2, column=0)
         options_c = ["NA", "1", "2", "3", "4", "5", "6"]
         self.prod_color_clicked = StringVar()
         self.prod_color_clicked.set(options_c[0])
-        self.prod_color = tk.OptionMenu(self , self.prod_color_clicked , *options_c)
-        self.prod_color.grid(row=2, column=1)
+        prod_color = tk.OptionMenu(frame1 , self.prod_color_clicked , *options_c)
+        prod_color.grid(row=2, column=1)
 
-        tk.Label(self, text="尺寸", font=font_title).grid(row=3, column=0)
+        tk.Label(frame1, text="尺寸", font=font_title, justify="right").grid(row=3, column=0)
         options_s = ["NA", "1號", "2號", "3號", "4號", "S", "M", "L", "XL"]
         self.prod_size_clicked = StringVar()
         self.prod_size_clicked.set(options_s[0])
-        self.prod_size = tk.OptionMenu(self , self.prod_size_clicked , *options_s)
-        self.prod_size.grid(row=3, column=1)
+        prod_size = tk.OptionMenu(frame1 , self.prod_size_clicked , *options_s)
+        prod_size.grid(row=3, column=1)
 
-        tk.Label(self, text="數量", font=font_title).grid(row=4, column=0)
-        self.prod_qty = tk.Entry(self, font=font_entry, width=6)
+        tk.Label(frame1, text="數量", font=font_title, justify="right").grid(row=4, column=0)
+        self.prod_qty = tk.Entry(frame1, font=font_entry, width=6)
         self.prod_qty.insert(tk.END, "1")
         self.prod_qty.grid(row=4, column=1)
 
-        tk.Label(self, text="姓名", font=font_title).grid(row=5, column=0)
-        self.entry_name = tk.Entry(self, font=font_entry)
-        self.entry_name.insert(tk.END, "王大明")
-        self.entry_name.grid(row=5, column=1)
+        box = ttk.Combobox(frame1, width=15, values=Constants.CONST_SIZE_LIST)
+        box.grid(row=5, column=1)
+        box.current(0)
+        # 改用這個box.current() box.get()
 
-        tk.Label(self, text="Email", font=font_title).grid(row=6, column=0)
-        self.entry_email = tk.Entry(self, font=font_entry)
-        self.entry_email.insert(tk.END, "example@example.com")
-        self.entry_email.grid(row=6, column=1)
+        frame1.button_clear_link = tk.Button(frame1, text="清除全部", command=self.clear_link_entry)
+        frame1.button_clear_link.grid(row=9, column=0, padx=4)
 
-        tk.Label(self, text="電話", font=font_title).grid(row=7, column=0)
-        self.entry_phone = tk.Entry(self, font=font_entry)
-        self.entry_phone.insert(tk.END, "0800092000")
-        self.entry_phone.grid(row=7, column=1)
-
-        tk.Label(self, text="Line ID", font=font_title).grid(row=8, column=0)
-        self.entry_line_id = tk.Entry(self, font=font_entry)
-        self.entry_line_id.insert(tk.END, "asdasdasd")
-        self.entry_line_id.grid(row=8, column=1)
+        self.add_notebook(frame1, "偏好設定")
 
     def clear_link_entry(self):
         self.entry_link.delete(0, tk.END)
 
-    def createSectionDeliver(self):
-        font_title = font.Font(family="Helvetica", size=12, weight="bold")
-        font_entry =  font.Font(family="Helvetica", size=12)
+    def create_section_autofill(self):
+        frame = tk.Frame()
+        font_area_start = font.Font(family="Helvetica", size=10, weight="bold")
+        font_title = font.Font(family="Helvetica", size=10)
+        font_entry =  font.Font(family="Helvetica", size=10)
 
-        tk.Label(self, text="門市店號", font=font_title).grid(row=15, column=0)
-        self.seven_id = tk.Entry(self, font=font_entry)
+        tk.Label(frame, text="基本資料", font=font_area_start).grid(row=1, column=0)
+
+        tk.Label(frame, text="姓名", font=font_title).grid(row=5, column=0)
+        self.entry_name = tk.Entry(frame, font=font_entry)
+        self.entry_name.insert(tk.END, "王大明")
+        self.entry_name.grid(row=5, column=1)
+
+        tk.Label(frame, text="Email", font=font_title).grid(row=6, column=0)
+        self.entry_email = tk.Entry(frame, font=font_entry)
+        self.entry_email.insert(tk.END, "example@example.com")
+        self.entry_email.grid(row=6, column=1)
+
+        tk.Label(frame, text="電話", font=font_title).grid(row=7, column=0)
+        self.entry_phone = tk.Entry(frame, font=font_entry)
+        self.entry_phone.insert(tk.END, "0800092000")
+        self.entry_phone.grid(row=7, column=1)
+
+        tk.Label(frame, text="Line ID", font=font_title).grid(row=8, column=0)
+        self.entry_line_id = tk.Entry(frame, font=font_entry)
+        self.entry_line_id.insert(tk.END, "asdasdasd")
+        self.entry_line_id.grid(row=8, column=1)
+
+        tk.Label(frame, text="送貨資訊", font=font_area_start).grid(row=11, column=0)
+        tk.Label(frame, text="門市店號", font=font_title).grid(row=12, column=0)
+        self.seven_id = tk.Entry(frame, font=font_entry)
         self.seven_id.insert(tk.END, "254063")
-        self.seven_id.grid(row=15, column=1)
+        self.seven_id.grid(row=12, column=1)
 
-    def createSectionCreditCard(self):
-        font_title = font.Font(family="Helvetica", size=12, weight="bold")
-        font_entry =  font.Font(family="Helvetica", size=12)
-
-        tk.Label(self, text="信用卡號(無空格)", font=font_title).grid(row=20, column=0)
-        self.cc_number = tk.Entry(self, font=font_entry)
+        tk.Label(frame, text="信用卡資料", font=font_area_start).grid(row=21, column=0)
+        tk.Label(frame, text="信用卡號(無空格)", font=font_title).grid(row=22, column=0)
+        self.cc_number = tk.Entry(frame, font=font_entry)
         self.cc_number.insert(tk.END, "4037200875496790")
-        self.cc_number.grid(row=20, column=1)
+        self.cc_number.grid(row=22, column=1)
 
-        tk.Label(self, text="到期年月(無斜線)", font=font_title).grid(row=21, column=0)
-        self.cc_date = tk.Entry(self, font=font_entry)
+        tk.Label(frame, text="到期年月(無斜線)", font=font_title).grid(row=23, column=0)
+        self.cc_date = tk.Entry(frame, font=font_entry)
         self.cc_date.insert(tk.END, "1229")
-        self.cc_date.grid(row=21, column=1)
+        self.cc_date.grid(row=23, column=1)
 
-        tk.Label(self, text="安全碼", font=font_title).grid(row=22, column=0)
-        self.cc_cvc = tk.Entry(self, font=font_entry)
+        tk.Label(frame, text="安全碼", font=font_title).grid(row=24, column=0)
+        self.cc_cvc = tk.Entry(frame, font=font_entry)
         self.cc_cvc.insert(tk.END, "777")
-        self.cc_cvc.grid(row=22, column=1)
+        self.cc_cvc.grid(row=24, column=1)
 
-        tk.Label(self, text="持卡人姓名", font=font_title).grid(row=23, column=0)
-        self.cc_name = tk.Entry(self, font=font_entry)
+        tk.Label(frame, text="持卡人姓名", font=font_title).grid(row=25, column=0)
+        self.cc_name = tk.Entry(frame, font=font_entry)
         self.cc_name.insert(tk.END, "王大明")
-        self.cc_name.grid(row=23, column=1)
+        self.cc_name.grid(row=25, column=1)
+
+        self.add_notebook(frame, "自動填入")
+
+    def create_section_about(self):
+        frame = tk.Frame()
+        banner = Image.open('res/goopi_bot.png') 
+        bannerPhotoImage  = ImageTk.PhotoImage(banner)
+
+        icon_label = tk.Label(frame, image=bannerPhotoImage)
+        icon_label.grid(row=0, column=0, columnspan=2)
+        icon_label.image = bannerPhotoImage
+        self.add_notebook(frame, "關於")
+
+    def create_footer_actions(self):
+        self.start_button = tk.Button(self, text="開啟瀏覽器", width=12, command=self.open_sign_in_page)
+        self.start_button.grid(row=2, column=0, pady=8)
+        self.start_button = tk.Button(self, text="執行", width=12, command=self.start_bot_action)
+        self.start_button.grid(row=2, column=1, pady=8)
 
     def open_sign_in_page(self):
         open_sign_in_page()
